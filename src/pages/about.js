fb0e0249bbc602b03fd8213c460e425956f08914
Kaleem elahi Shaikh <shaikh9996@gmail.com>
2019-01-22 17:06:03 +0530

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const AboutPage = () => (
+      <Layout>
+        <SEO title="Page two" />
+
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo laudantium exercitationem nisi distinctio consectetur cumque voluptatibus quasi facere adipisci rerum, porro totam corrupti corporis obcaecati ducimus sequi est quisquam iure ullam cum a natus reiciendis recusandae? Dolore corporis neque autem!</p>
+
+      </Layout>
+  )
+
+export default AboutPage
---
+++ src/pages/services.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const ServicesPage = () => (
+      <Layout>
+        <SEO title="Our Services" />
+
+        <h1>Our Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo laudantium exercitationem nisi distinctio consectetur cumque voluptatibus quasi facere adipisci rerum, porro totam corrupti corporis obcaecati ducimus sequi est quisquam iure ullam cum a natus reiciendis recusandae? Dolore corporis neque autem!</p>
+
+      </Layout>
+  )
+
+export default ServicesPage
