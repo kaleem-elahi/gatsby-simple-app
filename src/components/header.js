fae4b2ba4e0c7437c723100619b252f55df7e303
Kaleem elahi Shaikh <shaikh9996@gmail.com>
2019-02-04 19:30:57 +0530

    done till gatsby-node.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: `tomato`,
+      background: `grey`,
       marginBottom: `0`,
     }}
   >
---
+++ src/pages/2018-04-10-post-two/index.md
@@ -0,0 +1,8 @@
+---
+path: "/post-two"
+date: "2018-04-10"
+title: "My Second Gatsby Post"
+author: "John Doe"
+---
+
+This is my Second blog post in Gatsby
